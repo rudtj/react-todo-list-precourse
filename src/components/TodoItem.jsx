05d8b34e6515078e34658266f6603189bf59da58
rudtj <kyoungseo0327@gmail.com>
2024-06-05 09:33:24 +0900

    feat: 리스트 항목 기능 구현

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Checkbox from "./Checkbox";
+import DeleteBtn from "./DeleteBtn";
+import "./TodoItem.css";
+
+const TodoItem = ({ todo, onToggle, onDelete }) => {
+    return (
+        <div className="todo-item">
+            <Checkbox checked={todo.completed} onChange={() => onToggle(todo.id)} />
+            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+                {todo.text}
+            </span>
+            <DeleteBtn onDelete={() => onDelete(todo.id)} />
+        </div>
+    );
+}
+
+export default TodoItem;
