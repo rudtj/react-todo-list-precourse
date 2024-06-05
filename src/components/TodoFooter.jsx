e4e06d45363784cde32d8db9bff3c6ebd3e4c7fa
rudtj <kyoungseo0327@gmail.com>
2024-06-05 09:36:14 +0900

    refactor: 파일 경로 수정

--- src/components/TodoFooter.jsx
+++ src/components/TodoFooter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./TodoFooter.css";
+import "../styles/TodoFooter.css";
 
 const TodoFooter = ({ todosCount, filter, setFilter, clearCompleted }) => {
     return (
