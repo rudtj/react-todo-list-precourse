37ecb9b4c91a00563ca61e3ff4dd55160c19aaf6
rudtj <kyoungseo0327@gmail.com>
2024-06-05 09:35:10 +0900

    feat: footer 기능 구현

---
+++ src/components/TodoFooter.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./TodoFooter.css";
+
+const TodoFooter = ({ todosCount, filter, setFilter, clearCompleted }) => {
+    return (
+        <div className="todo-footer">
+          <span>{todosCount} items left!</span>
+          <div>
+            <button onClick={() => setFilter('all')} className="action-btn">All</button>
+            <button onClick={() => setFilter('active')} className="action-btn">Active</button>
+            <button onClick={() => setFilter('completed')} className="action-btn">Completed</button>
+          </div>
+          <button onClick={clearCompleted} className="clear-btn">Clear completed</button>
+        </div>
+    );
+}
+
+export default TodoFooter;
