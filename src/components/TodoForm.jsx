5fd6189899f0e98466bae612169dfd1afeea5882
rudtj <kyoungseo0327@gmail.com>
2024-06-05 09:27:50 +0900

    feat: 입력창 기능 구현

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import "./TodoForm.css";
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (value.trim().length) {
+      addTodo(value);
+      setValue("");
+    }
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        placeholder="What needs to be done?"
+      />
+    </form>
+  );
+}
+
+export default TodoForm;
