571cd22fe2bc5fd008be832f9e123ab24561f197
rudtj <kyoungseo0327@gmail.com>
2024-06-05 09:32:28 +0900

    refactor: 파일 경로 수정

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import TodoItem from './TodoItem';
-import "./TodoList.css";
+import "../styles/TodoList.css";
 
 const TodoList = ({ todos, onToggle, onDelete }) => {
   return (
